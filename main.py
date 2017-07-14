b62fb227fdd52d7c615a6567b9d2bd3143bac517
markwiliz30 <markwiliz30@yahoo.com>
2017-07-14 23:48:08 +0800

    Changed again

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python2.7
 def main(msg):
-<<<<<<< HEAD
     // Adding a different comment to this file.
-=======
     // Print a message
->>>>>>> refs/remotes/origin/master
     print(msg)
 
 main("hello people")
