d589266732f7d0c57261d53df3481a3ecf055257
markwiliz30 <markwiliz30@yahoo.com>
2017-07-14 23:54:28 +0800

    Newest commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-pi-example
 This is an example project to demonstrate the use of git in Mint.
+
+Make a change
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2.7
 def main(msg):
+    //Another comment
     // Adding a different comment to this file.
     // Print a message
     print(msg)
